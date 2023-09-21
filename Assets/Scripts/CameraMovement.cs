166cce79cfbc6f3c7ec88b267aa4fdd7fa45d731
PhuocHoang <htphuoc1995@gmail.com>
2023-09-21 13:40:53 -0400

    Chapter 3 - Add camera movement, gun scripts and projectile prefab

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject followTarget;
+    public float moveSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followTarget != null)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                followTarget.transform.position, Time.deltaTime * moveSpeed);
+        }
+    }
+}
