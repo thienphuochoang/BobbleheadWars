bec97ac709bd353475cca8d56c3622258c64177d
PhuocHoang <htphuoc1995@gmail.com>
2023-09-21 13:29:37 -0400

    Chapter 3 - Add new player controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 50.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position = pos;
+    }
+}
