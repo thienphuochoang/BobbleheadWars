564d719775a466b3628a52e69f1b759a275b0785
PhuocHoang <htphuoc1995@gmail.com>
2023-09-28 21:24:14 -0400

    Chapter 5 - Add Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] spawnPoints;
+    public GameObject alien;
+    public int maxAliensOnScreen;
+    public int totalAliens;
+    public float minSpawnTime;
+    public float maxSpawnTime;
+    public int aliensPerSpawn;
+    private int aliensOnScreen = 0;
+    private float generatedSpawnTime = 0;
+    private float currentSpawnTime = 0;
+
+    private void Update()
+    {
+        currentSpawnTime += Time.deltaTime;
+        if (currentSpawnTime > generatedSpawnTime)
+        {
+            currentSpawnTime = 0;
+            generatedSpawnTime = UnityEngine.Random.Range(minSpawnTime, maxSpawnTime);
+            if (aliensPerSpawn > 0 && aliensOnScreen < totalAliens)
+            {
+                List<int> previousSpawnLocations = new List<int>();
+                if (aliensPerSpawn > spawnPoints.Length)
+                {
+                    aliensPerSpawn = spawnPoints.Length - 1;
+                }
+                aliensPerSpawn = (aliensPerSpawn > totalAliens) ?
+                    aliensPerSpawn - totalAliens : aliensPerSpawn;
+                for (int i = 0; i < aliensPerSpawn; i++)
+                {
+                    if (aliensOnScreen < maxAliensOnScreen)
+                    {
+                        aliensOnScreen += 1;
+                        // 1
+                        int spawnPoint = -1;
+                        // 2
+                        while (spawnPoint == -1)
+                        {
+                            // 3
+                            int randomNumber = UnityEngine.Random.Range(0, spawnPoints.Length - 1);
+                            // 4
+                            if (!previousSpawnLocations.Contains(randomNumber))
+                            {
+                                previousSpawnLocations.Add(randomNumber);
+                                spawnPoint = randomNumber;
+                            }
+                        }
+                        GameObject spawnLocation = spawnPoints[spawnPoint];
+                        GameObject newAlien = Instantiate(alien) as GameObject;
+                        newAlien.transform.position = spawnLocation.transform.position;
+                    }
+                }
+            }
+        }
+    }
+}
